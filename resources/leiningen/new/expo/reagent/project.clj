d28c7b2525763835f26620631e3bb912515904e9
Supakorn Warodom <supakorn@satang.com>
2018-12-02 19:39:37 +0700

    Update expo to 30.0.0

--- resources/leiningen/new/expo/reagent/project.clj
+++ resources/leiningen/new/expo/reagent/project.clj
@@ -4,23 +4,20 @@
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
-                 [binaryage/oops "0.5.8"]
-                           [reagent "0.7.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server cljsjs/create-react-class]]
-                           [re-frame "0.9.3"]
-                           [react-native-externs "0.1.0"]]
-            :plugins [[lein-cljsbuild "1.1.4"]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [binaryage/oops "0.6.3"]
+                 [reagent "0.7.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server cljsjs/create-react-class]]
+                 [re-frame "0.10.6"]
+                 [react-native-externs "0.2.0"]]
+            :plugins [[lein-cljsbuild "1.1.7"]
                       [lein-figwheel "0.5.14"]]
             :clean-targets ["target/" "main.js"]
             :aliases {"figwheel"        ["run" "-m" "user" "--figwheel"]
-                      ; TODO: Remove custom extern inference as it's unreliable
-                      ;"externs"         ["do" "clean"
-                      ;                   ["run" "-m" "externs"]]
                       "rebuild-modules" ["run" "-m" "user" "--rebuild-modules"]
                       "prod-build"      ^{:doc "Recompile code with prod profile."}
                                         ["with-profile" "prod" "cljsbuild" "once" "main"]}
             :profiles {:dev  {:dependencies [[figwheel-sidecar "0.5.14"]
-                                             [com.cemerick/piggieback "0.2.1"]]
+                                             [com.cemerick/piggieback "0.2.2"]]
                               :source-paths ["src" "env/dev"]
                               :cljsbuild    {:builds [{:id           "main"
                                                        :source-paths ["src" "env/dev"]
